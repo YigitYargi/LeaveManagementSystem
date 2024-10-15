99ea9cfa18f00603c888dc3c546f032606a226df
YigitYargi <yigityargi26@gmail.com>
2024-10-15 15:03:17 +0300

    Proje dosyası ekle.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeaveManagementSystem.Web.Models;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(2001,07,25)
+            };
+            return View(data);
+        }
+    }
+}
